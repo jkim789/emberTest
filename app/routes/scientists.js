228a6df5c84779b15758d472a5045d5b529d74a7
Joon Kim <jkim7078@gmail.com>
2023-02-22 08:02:03 -0500

    initial commit

---
+++ app/routes/scientists.js
@@ -0,0 +1,12 @@
+import Route from '@ember/routing/route';
+
+export default class ScientistsRoute extends Route {
+
+  model() {
+    return [
+      'Marie Curie',
+      'Mae Jemison',
+      'Albert Hofmann'
+    ];
+  }
+}
